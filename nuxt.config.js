300337b1ec140f016c3ac823e581dbc56f0a9a05
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-01-21 21:13:54 +0700

    add sample meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,10 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' },
+
+      // example vue-meta
+      { hid: 'title', name: 'title', content: 'Nuxt.js project' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
