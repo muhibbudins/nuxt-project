ee615f47f1db1ca6e4211c4af602cb09ee5e9bc5
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-01-22 07:31:27 +0700

    add swiper js on banner

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -1,8 +1,34 @@
 <template>
   <div>
-    <h4>Ini header</h4>
-    <nuxt/>
-    <h4>Ini side bar</h4>
+    <!-- Navigation -->
+    <Navigation/>
+    <!-- Content -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-9 col-lg-9">
+          <!-- Content -->
+          <Banner />
+          <nuxt/>
+        </div>
+        <div class="col-md-3 col-lg-3">
+          <!-- Sidebar -->
+          <h4>Ini side bar</h4>
+        </div>
+      </div>
+    </div>
+    <!-- Footer -->
     <h4>Ini footer</h4>
   </div>
 </template>
+
+<script>
+import Navigation from '~/components/navigation.vue'
+import Banner from '~/components/banner.vue'
+
+export default {
+  components: {
+    Navigation,
+    Banner
+  }
+}
+</script>
